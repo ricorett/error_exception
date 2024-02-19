0a4f1ad909810117c2068218c0432f1d97093364
Lana <115147586+ricorett@users.noreply.github.com>
2024-02-19 14:30:42 +0300

    done 01 task idk

--- 01/main.cpp
+++ 01/main.cpp
@@ -1,23 +1,33 @@
-#include "main.h"
+#include <iostream>
+#include <exception>
+
+int function(std::string str, int forbidden_length);
 
 int main(){
-    int forbidden_lenght;
     std::string str;
-    std::cout << "Введите запретную длину:";
-    std::cin >> forbidden_lenght;
+    int forbidden_length = 0;
+    std::cout << "Введите запретную длину: ";
+    std::cin >> forbidden_length;
+
+    try {
+        while (true) {
+            std::cout << "Введите слово: ";
+            std::cin >> str;
+            int str_size = function(str, forbidden_length);
+            std::cout << "Длина слова \"" << str << "\" равна " << str.length() << std::endl;
+        }
+    }
+        catch (std::length_error &err) {
+            std::cout << "Ошибка: " << err.what();
+        }
 
     return 0;
 }
 
 
-int function(std::string str, int forbidden_lenght){
-try
-{
-while(str.size() != forbidden_lenght){
-std::cin >> forbidden_lenght;
-}
-
-}
-catch();
-return OK;
+int function(std::string str, int forbidden_length){
+    if(str.length() == forbidden_length){
+        throw std::length_error("Вы ввели слово запретной длины! До свидания!");
+    }
+    return str.length();
 }
--- 01/main.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <exception>
-
-enum errors{
-OK,
-BAD_LENGHT
-};
-
-int function(std::string str, int forbidden_lenght);
